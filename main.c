5a4041317cfd9e913f0c6d45c5ee09c5b1e46afc
Zachary W. Graham <zwgraham@soe.ucsc.edu>
2016-04-30 01:52:34 -0700

    Added a debug uart
    this uart will print messages out the USB-connected uart (UART0). The radio is
    connected UART1, inestead

--- main.c
+++ main.c
@@ -10,14 +10,37 @@
 #include "uart.h"
 #include "parser.h"
 
-#define UART_BAUD 19200
+#define RADIO_BAUD 19200
+#define DEBUG_BAUD 115200
+#define DEBUG_UART   uart
+#define RADIO_UART   uart1
+/* only the above macros should be edited for configuration purposes */
+#define xCONCAT(a, b) a##b
+#define CONCAT(a, b) xCONCAT(a, b)
+#define INIT _init
+#define PUTS _puts
+#define PUTS_p _puts_p
+#define PUTS_P _puts_P
+
+#define RADIO_INIT   CONCAT(RADIO_UART, INIT)
+#define RADIO_PUTS   CONCAT(RADIO_UART, PUTS)
+#define RADIO_PUTS_p CONCAT(RADIO_UART, PUTS_p)
+#define RADIO_PUTS_P CONCAT(RADIO_UART, PUTS_P)
+#define DEBUG_INIT   CONCAT(DEBUG_UART, INIT)
+#define DEBUG_PUTS   CONCAT(DEBUG_UART, PUTS)
+#define DEBUG_PUTS_p CONCAT(DEBUG_UART, PUTS_p)
+#define DEBUG_PUTS_P CONCAT(DEBUG_UART, PUTS_P)
 
 
 static void
 serial_init(void){
-    uart_init(UART_BAUD_SELECT(UART_BAUD, F_CPU));
+#ifdef DEBUG
+    DEBUG_INIT(UART_BAUD_SELECT(DEBUG_BAUD, F_CPU));
+#endif
+    RADIO_INIT(UART_BAUD_SELECT(RADIO_BAUD, F_CPU));
 }
 
+#define COMMAND_ERROR "BAD COMMAND\r\n"
 
 int
 main(void){
@@ -36,27 +59,43 @@ main(void){
         if( parser_flags.command_error ){
             /* send error message */
             parser_flags.command_error=0;
-            uart_puts_P("BAD COMMAND\r\n");
+#ifdef DEBUG
+            DEBUG_PUTS_P("MAIN: parser_flags.command_error=1\r\n");
+#endif
+            uart_puts_P(COMMAND_ERROR);
         }
         if( parser_flags.measure_temperature &&
             parser_flags.measure_humidity ){
             dht_read_data( &d, &temp, &hum);
             sprintf_P(buf, PSTR("T=%0.2f:H=%0.2f\r\n"), temp, hum);
+#ifdef DEBUG
+            DEBUG_PUTS_P("MAIN: measure_temperature && measure_humidity\r\n");
+            DEBUG_PUTS(buf);
+#endif
             parser_flags.measure_temperature=0;
             parser_flags.measure_humidity=0;
         }
         else if( parser_flags.measure_temperature ){
             dht_read_data( &d, &temp, &hum);
             sprintf_P(buf, PSTR("T=%0.2f\r\n"), temp);
+#ifdef DEBUG
+            DEBUG_PUTS_P("MAIN: measure_temperature\r\n");
+            DEBUG_PUTS(buf);
+#endif
+
             parser_flags.measure_temperature=0;
         }
         else if( parser_flags.measure_humidity ){
             dht_read_data( &d, &temp, &hum);
             sprintf_P(buf, PSTR("H=%0.2f\r\n"), hum);
+#ifdef DEBUG
+            DEBUG_PUTS_P("MAIN: measure_humidity\r\n");
+            DEBUG_PUTS(buf);
+#endif
             parser_flags.measure_humidity=0;
         }
         if(buf[0]){
-            uart_puts(buf);
+            RADIO_PUTS(buf);
             buf[0]='\0';
         }
     }
