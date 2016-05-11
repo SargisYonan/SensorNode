49f01019f9c9b7fd00150f839a2514721f09ece7
Zachary W. Graham <zwgraham@soe.ucsc.edu>
2016-05-11 15:03:28 -0700

    Re-tabbed code
    4-space tabs, instead of 3 for consistency

--- src/parser.c
+++ src/parser.c
@@ -13,78 +13,78 @@ char parser_buffer[UART_RX_BUFFER_SIZE];
 uint8_t pb_index=0;
 
 void process_uart(void){
-   uint16_t c;
-   c = RADIO_GETC();
-   if( (c & UART_FRAME_ERROR) ||
-         (c & UART_OVERRUN_ERROR) ||
-         (c & UART_BUFFER_OVERFLOW) ){
-      /* set an error flag */
-      parser_flags.uart_error = 1;
-      /* should we reset the buffer? */
-      return;
-   }
+    uint16_t c;
+    c = RADIO_GETC();
+    if( (c & UART_FRAME_ERROR) ||
+            (c & UART_OVERRUN_ERROR) ||
+            (c & UART_BUFFER_OVERFLOW) ){
+        /* set an error flag */
+        parser_flags.uart_error = 1;
+        /* should we reset the buffer? */
+        return;
+    }
 
-   /* there's a uart character to process */
-   if(!(c & UART_NO_DATA)){
-      if (c == '\n'){
-         // do we do something if pb_index = 0 at this point?
-         // --> probably not since it will be parsed as a bad command by parse_command
-         parser_flags.command_recieved=1;
-         memcpy(parser_buffer, uart_buffer, pb_index);
-         parser_buffer[pb_index]=0;
+    /* there's a uart character to process */
+    if(!(c & UART_NO_DATA)){
+        if (c == '\n'){
+            // do we do something if pb_index = 0 at this point?
+            // --> probably not since it will be parsed as a bad command by parse_command
+            parser_flags.command_recieved=1;
+            memcpy(parser_buffer, uart_buffer, pb_index);
+            parser_buffer[pb_index]=0;
 #ifdef DEBUG
-         DEBUG_PUTS_P("Process_Uart: detected newline character\r\n\tcommand: ");
-         DEBUG_PUTS(parser_buffer);
-         DEBUG_PUTS_P("\r\n");
+            DEBUG_PUTS_P("Process_Uart: detected newline character\r\n\tcommand: ");
+            DEBUG_PUTS(parser_buffer);
+            DEBUG_PUTS_P("\r\n");
 #endif /* DEBUG */
-         pb_index=0;
-         /*
-          * resetting the index should allow us to avoid
-          * memsetting the buffer
-          */
-         return;
-      }
-      uart_buffer[pb_index++]= (unsigned char) c; /* lose those uart status flags */
+            pb_index=0;
+            /*
+             * resetting the index should allow us to avoid
+             * memsetting the buffer
+             */
+            return;
+        }
+        uart_buffer[pb_index++]= (unsigned char) c; /* lose those uart status flags */
 
-      /* bound checking if we recieve way to many characters drop the buffer */
-      if(pb_index >= UART_RX_BUFFER_SIZE){
-         /* clear the buffer and send a command error */
-         //parser_flags.command_error=1;
-         pb_index=0;
-      }
-   }
+        /* bound checking if we recieve way to many characters drop the buffer */
+        if(pb_index >= UART_RX_BUFFER_SIZE){
+            /* clear the buffer and send a command error */
+            //parser_flags.command_error=1;
+            pb_index=0;
+        }
+    }
 }
 
 void parse_command(void) {
-   /* look at queries */
+    /* look at queries */
 #ifdef DHT_SENSOR
-   if(strcasestr(parser_buffer, "T?")){
-      parser_flags.measure_temperature=1;
-   }
-   else if(strcasestr(parser_buffer, "H?")){
-      parser_flags.measure_humidity=1;
-   }
+    if(strcasestr(parser_buffer, "T?")){
+        parser_flags.measure_temperature=1;
+    }
+    else if(strcasestr(parser_buffer, "H?")){
+        parser_flags.measure_humidity=1;
+    }
 #endif /* DHT_SENSOR */
 #ifdef TEMP_SENSOR
-   if(strcasestr(parser_buffer, "T?")){
-      parser_flags.measure_temperature=1;
-   }
+    if(strcasestr(parser_buffer, "T?")){
+        parser_flags.measure_temperature=1;
+    }
 #endif /* TEMP_SENSOR */
 #ifdef LIGHT_SENSOR
-   if(strcasestr(parser_buffer, "L?")){
-      parser_flags.measure_light=1;
-   }
+    if(strcasestr(parser_buffer, "L?")){
+        parser_flags.measure_light=1;
+    }
 #endif /* LIGHT_SENSOR */
-   else if(strcasestr(parser_buffer, "READ")){
+    else if(strcasestr(parser_buffer, "READ")){
 #ifdef DHT_SENSOR
-      parser_flags.measure_temperature=1;
-      parser_flags.measure_humidity=1;
+        parser_flags.measure_temperature=1;
+        parser_flags.measure_humidity=1;
 #endif /* DHT_SENSOR */
 #ifdef TEMP_SENSOR
-      parser_flags.measure_temperature=1;
+        parser_flags.measure_temperature=1;
 #endif /* TEMP_SENSOR */
 #ifdef LIGHT_SENSOR
-      parser_flags.measure_light=1;
+        parser_flags.measure_light=1;
 #endif /* LIGHT_SENSOR */
     }
     else if (strcasestr(parser_buffer, "S=")) {
--- src/parser.h
+++ src/parser.h
@@ -5,22 +5,22 @@ void process_uart(void);
 void parse_command(void);
 
 struct flags{
-   uint8_t uart_error:1;
-   uint8_t command_error:1;
-   uint8_t command_recieved:1;
-   uint8_t command_error_setpoint:1;
-   uint8_t set_setpoint:1;
-   uint8_t get_setpoint:1;
-   uint16_t var_setpoint;
+    uint8_t uart_error:1;
+    uint8_t command_error:1;
+    uint8_t command_recieved:1;
+    uint8_t command_error_setpoint:1;
+    uint8_t set_setpoint:1;
+    uint8_t get_setpoint:1;
+    uint16_t var_setpoint;
 #ifdef DHT_SENSOR
-   uint8_t measure_temperature:1;
-   uint8_t measure_humidity:1;
+    uint8_t measure_temperature:1;
+    uint8_t measure_humidity:1;
 #endif /* DHT */
 #ifdef TEMP_SENSOR
-   uint8_t measure_temperature:1;
+    uint8_t measure_temperature:1;
 #endif /* TEMP */
 #ifdef LIGHT_SENSOR
-   uint8_t measure_light:1;
+    uint8_t measure_light:1;
 #endif /* LIGHT */
 } parser_flags;
 #endif /* _PARSER_H_ */
