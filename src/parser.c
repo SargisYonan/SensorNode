92b57557f37da67c968e527b6705a61f4a0f8c6d
legendddhgf <legendddhgf@gmail.com>
2017-03-16 03:02:28 -0700

    Parser library working

--- src/parser.c
+++ src/parser.c
@@ -1,14 +1,14 @@
 #include "parser.h"
-
+#include <string.h>
 #include <ctype.h>
-
-#include "uart.h"
+#include "common.h"
 
 // returns a Parser object with relevant information
 // If there is an erroneous input, cmd will be set to the null character
-Parser parse(char *input_str) {
+Parser parse_cmd(char *input_str) {
   Parser p;
   p.cmd = input_str[0]; // command is first character
+  p.ret_str = "";
   const char delimit[4] = " \r\n";
   const char *token = strtok(input_str, delimit);
   uint8_t device_index = -1;
@@ -17,7 +17,7 @@ Parser parse(char *input_str) {
     return p;
   }
   switch(token[0]) {
-    case 'c': // TODO: handle multiple pins for one device?
+    case CHAR_CREATE: // TODO: handle multiple pins for one device?
       token = strtok(NULL, delimit);
       if (!token) { // no arguments
         p.cmd = '\0';
@@ -33,42 +33,61 @@ Parser parse(char *input_str) {
         p.cmd = '\0';
         return p;
       }
-      if (!isalpha(token[1] || !isdigit(token[2]))) { // not following "PXN"
+      if (!isupper(token[1]) || !isdigit(token[2])) { // not following "PXN"
         p.cmd = '\0';
         return p;
       }
       p.address_index = (uint8_t) token[1] - 'A'; // 'A'-'H' : 0-7
       p.reg_bit = (uint8_t) token[1] - '0'; // '0'-'7' : 0-7
       break;
-    case 'i':
+    case CHAR_INIT:
       if (!isdigit(token[1])) { // this isn't a number (but it should be)
         p.cmd = '\0';
         return p;
       }
-      device_index = (uint8_t) atoi(token + 1);
+      device_index = (uint8_t) atoi(token + 1); // index is second character
       p.device_index = device_index;
       break;
-    case 'r':
+    case CHAR_READ:
       if (!isdigit(token[1])) { // this isn't a number (but it should be)
         p.cmd = '\0';
         return p;
       }
-      device_index = (uint8_t) atoi(token + 1);
+      device_index = (uint8_t) atoi(token + 1); // index is second character
       p.device_index = device_index;
       break;
-    case 'w':
-      break;
-    case 'd':
+    case CHAR_WRITE:
       if (!isdigit(token[1])) { // this isn't a number (but it should be)
         p.cmd = '\0';
         return p;
       }
-      device_index = (uint8_t) atoi(token + 1);
+      device_index = (uint8_t) atoi(token + 1); // index is second character
+      p.device_index = device_index;
+      token = strtok(NULL, delimit);
+      if (!token) { // no string to write
+        p.cmd = '\0';
+        return p;
+      }
+      p.ret_str = token; // ret_str is string to write
+      break;
+    case CHAR_DESTROY:
+      if (!isdigit(token[1])) { // this isn't a number (but it should be)
+        p.cmd = '\0';
+        return p;
+      }
+      device_index = (uint8_t) atoi(token + 1); // index is second character
       p.device_index = device_index;
       break;
-    case 'k':
+    case CHAR_KILL:
+      if (!isdigit(token[1])) { // this isn't a number (but it should be)
+        p.cmd = '\0';
+        return p;
+      }
+      device_index = (uint8_t) atoi(token + 1); // index is second character
+      p.device_index = device_index;
       break;
-    case 'm':
+    case CHAR_MAP:
+      // nothing to do
       break;
     default:
       p.cmd = '\0'; // invalid command
